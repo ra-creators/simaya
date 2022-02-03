fa5dc08b5e68e1064879cec84f46e4f99e3000a5
Jay Kumar Gaur <jaygaur99@gmail.com>
2022-02-03 20:48:58 +0530

    Fixed some merge errors

--- simaya/settings.py
+++ simaya/settings.py
@@ -105,6 +105,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'django_db',
+    #     'USER': 'root',
+    #     'PASSWORD': 'Bhavesh@8817',
+    #     'HOST':'localhost',
+    #     'PORT':'3306',
+    # }
 }
 
 
